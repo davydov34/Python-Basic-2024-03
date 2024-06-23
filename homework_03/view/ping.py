5741bfa8089cabfe330c0d5b8cbaa8b41928e8c3
user1 <user1.kaustik.ru>
2024-06-23 21:54:52 +0300

    Corrected in ping.py

--- homework_03/view/ping.py
+++ homework_03/view/ping.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 router = APIRouter()
 
-@router.get("/ping/", status_code=200, response_class=JSONResponse, tags=["ping"])
+@router.get("/ping/", status_code=200, tags=["ping"])
 def func_ping():
-    return { "message": "pong" }
+    data = {"message": "pong"}
+    json_data = jsonable_encoder(data)
+    return JSONResponse(content=json_data)
